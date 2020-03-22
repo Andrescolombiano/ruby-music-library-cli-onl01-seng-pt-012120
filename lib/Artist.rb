3202053fdc5be887829d78d5ec932920abd3090c
Andres Carvajal <jc1993box@hotmail.com>
2020-03-22 16:45:46 +0000

    Automatically backed up by Learn

---
+++ lib/Artist.rb
@@ -0,0 +1,43 @@
+class Artist 
+
+attr_accessor :name, :songs, :artist
+
+@@all = []
+
+def initialize(name)
+  @name = name 
+  @songs = []
+end 
+
+def songs
+  @songs
+end 
+
+def add_song(song)
+ song.artist = self unless song.artist
+ @songs << song unless @songs.include?(song)
+end 
+
+def genres 
+songs.collect{|song|song.genre}.uniq
+end 
+
+def self.all
+  @@all
+end 
+
+def self.destroy_all
+  @@all.clear
+end 
+
+def save
+  @@all << self 
+end 
+
+def self.create (name)
+  artist = new(name)
+  artist.save
+  artist
+end
+
+end 
